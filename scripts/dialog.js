e65c2ff050708b4686763e669898ab1900e0c24b
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-07-07 00:59:32 +0400

    formatting

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,13 +1,13 @@
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("#filter-modal-open-button");
-const closeButton = document.querySelector("#filter-modal-close-button");
+const dialog = document.querySelector('dialog')
+const showButton = document.querySelector('#filter-modal-open-button')
+const closeButton = document.querySelector('#filter-modal-close-button')
 
 // "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
-  dialog.showModal();
-});
+showButton.addEventListener('click', () => {
+  dialog.showModal()
+})
 
 // "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-  dialog.close();
-});
+closeButton.addEventListener('click', () => {
+  dialog.close()
+})
