b5cf0e16ebd78cd37b674dd08abdc74af79f3631
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-07-08 01:42:21 +0400

    adding info-modals

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,3 +1,4 @@
+const dialogs = document.querySelectorAll('dialog')
 const filterDialog = document.querySelector('#filter-modal')
 const showButton = document.querySelector('#filter-modal-open-button')
 const closeButton = document.querySelector('#filter-modal-close-button')
@@ -12,8 +13,40 @@ closeButton.addEventListener('click', () => {
   filterDialog.close()
 })
 
-filterDialog.addEventListener('mousedown', (event) => {
-  if (event.target === filterDialog) {
-    filterDialog.close()
-  }
+dialogs.forEach((dialog) =>
+  dialog.addEventListener('mousedown', (event) => {
+    if (event.target === dialog) {
+      dialog.close()
+    }
+  })
+)
+
+const subscribeForm = document.querySelector('#subscribe-form')
+const successDialog = document.querySelector('#success-modal')
+const errorDialog = document.querySelector('#error-modal')
+const errorDialogCloseButton = document.querySelector(
+  '#error-modal-close-button'
+)
+
+let didSubmit = false
+
+subscribeForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  didSubmit = true
+})
+
+const subscribeButton = subscribeForm.querySelector('button')
+subscribeButton.addEventListener('mousedown', () => {
+  didSubmit = false
+  setTimeout(() => {
+    if (didSubmit) {
+      successDialog.showModal()
+    } else {
+      errorDialog.showModal()
+    }
+  }, 200)
+})
+
+errorDialogCloseButton.addEventListener('click', () => {
+  errorDialog.close()
 })
--- scripts/fields.js
+++ scripts/fields.js
@@ -4,14 +4,6 @@ errorableFields.forEach((field) => {
   const input = field.querySelector('input')
 
   input.addEventListener('blur', () => {
-    if (input.value === '') {
-      field.classList.add('error')
-    } else {
-      field.classList.remove('error')
-    }
-  })
-
-  input.addEventListener('keydown', () => {
-    field.classList.remove('error')
+    field.classList.add('touched')
   })
 })
